4a29ac79f9c0dada7fae6446e3f4d2a2e9d77ea6
Mostafa Shamin Yeasar <mostafa.shaminbjit@gmail.com>
2022-09-06 08:38:40 +0600

    Initial Commit

---
+++ AboutFruits/AboutFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutFruitsApp.swift
+//  AboutFruits
+//
+//  Created by BJIT on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AboutFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
